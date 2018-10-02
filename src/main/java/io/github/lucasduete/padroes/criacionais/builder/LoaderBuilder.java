10c31915b6d50cf756fe83f1d052556eb3ad2a87
Lucas Duete <lm.duete@gmail.com>
2018-10-02 11:08:49 -0300

    Adicionando casos de exceções na classe Loader para exemplificação.

--- src/main/java/io/github/lucasduete/padroes/criacionais/builder/LoaderBuilder.java
+++ src/main/java/io/github/lucasduete/padroes/criacionais/builder/LoaderBuilder.java
@@ -3,8 +3,34 @@ package io.github.lucasduete.padroes.criacionais.builder;
 public class LoaderBuilder {
 
     public static void main(String[] args) {
-        Pizza pizza = new PizzaBuilder().comMassaPan().comMussarela().comAzeitora().build();
+//        Pizza pizza = new PizzaBuilder().comMassaPan().comMussarela().comAzeitora().build();
+//        System.out.println(pizza);
 
-        System.out.println(pizza);
+//        Pizza com Duas Massas
+//        Pizza pizza = new PizzaBuilder().comMassaTradicional().comMassaPan().comAzeitora().build();
+
+//        Quantidade de Ingredientes Acima do Limite
+//        Pizza pizza = new PizzaBuilder()
+//                .comMassaTradicional()
+//                .comBacon()
+//                .comAzeitora()
+//                .comBacon()
+//                .comMussarela()
+//                .comBacon()
+//                .comManjericão()
+//                .comBacon()
+//                .comTomate()
+//                .comBacon()
+//                .comCalabresa()
+//                .comBacon()
+//                .comBacon()
+//                .build();
+
+//        Nenhuma Massa Definida
+//        Pizza pizza = new PizzaBuilder().comBacon().build();
+
+//        Nenhum Ingrediente Definido
+//        Pizza pizza = new PizzaBuilder().comMassaTradicional().build();
+
     }
 }
